3fb1e0015eeac5ed7eadd968334571afe026e55b
fincharts <fincharts.gross@gmail.com>
2019-08-06 21:18:10 +0200

    --

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-//
+const request = require ( 'request' );
+
+exports.info = function ( key ) {
+
+	let api_key = process.env.key || key, // signup for free at exactip.gogross.com
+	    url      = 'https://api.gogross.com/ip/?key=' + api_key;
+	
+	request ( { url : url }, function ( error, response, body ) {
+		
+		if ( error ) {
+			
+			throw error
+			
+		} else {
+			
+			return body
+			
+		}
+		
+	} );
+	
+};
